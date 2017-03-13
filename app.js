6763a3084b8367f6a70f8b2c13af3f74bbd3e837
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-13 13:57:01 +0100

    afficher les valeurs 1 et 2

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-console.log ("salut");
+$('document').ready(function(){
+	var foo="";
+	var bar="";
+	var result="";
+
+
+
+//Entrée de la variable 1
+
+
+	$('.valeur1').click(function(){
+		bar=bar + $(this).html();
+		console.log(bar);
+		console.log(typeof bar);
+		$('input').val(bar);
+	});
+
+
+
+// Entrée de la variable 2
+
+	$('.valeur2').click(function(){
+		foo=foo +$(this).html();
+		console.log(foo);
+		console.log(typeof foo);
+		$('input').val(foo);
+	});
+});
+
+/* 
+	function addition (a,b ) {
+		return(a+b); 
+	};
+
+	console.log (addition(2,4)); 
+
+
+
+	function substraction(a,b) {
+		return(a-b);
+	};
+
+	console.log (substraction(6,1));
+
+	function multiplication(a,b) {
+		return(a*b);
+	};
+
+	console.log (multiplication(9,4));
+
+
+
+	function division(a,b) {
+		if (b > 0)
+			console.log(a/b);
+		else
+			console.log("Can't divide by 0");}
+
+		division(21,4);
+
+*/
+
+
+	
+
